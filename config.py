43de0d4891df9629643014b6be8cfd33f3322a06
alexcbb <alexchapin@hotmail.fr>
2024-01-28 15:20:46 +0100

    Set back config file

---
+++ config.py
@@ -0,0 +1,27 @@
+class CFG:
+    ycb_path = './dataset/ycb/'
+    ycb_train = ycb_path + 'train_real/'
+    ycb_test = ycb_path + 'test/'
+    
+    max_len = 300
+    img_size = 384
+    num_bins = img_size
+    
+    batch_size = 1024
+    epochs = 30
+    precision = 16
+    gpus = 4
+    nodes = 1
+    strategy = "ddp"
+    
+    model_name = 'deit3_small_patch16_384_in21ft1k'
+    num_patches = 576
+    
+    lr = 1e-4
+    weight_decay = 1e-4
+    
+    num_classes = 21
+    num_workers = 8
+    
+    generation_steps = 101
+    run_eval = False
--- train.py
+++ train.py
@@ -8,36 +8,7 @@ from models import Encoder, Decoder, Pix2Seq
 from data.datamodule import YCBDatamodule
 import string
 import random
-
-
-
-class CFG:
-    ycb_path = './dataset/ycb/'
-    ycb_train = ycb_path + 'train_real/'
-    ycb_test = ycb_path + 'test/'
-    
-    max_len = 300
-    img_size = 384
-    num_bins = img_size
-    
-    batch_size = 1024
-    epochs = 30
-    precision = 16
-    gpus = 4
-    nodes = 1
-    strategy = "ddp"
-    
-    model_name = 'deit3_small_patch16_384_in21ft1k'
-    num_patches = 576
-    
-    lr = 1e-4
-    weight_decay = 1e-4
-    
-    num_classes = 21
-    num_workers = 8
-    
-    generation_steps = 101
-    run_eval = False
+from config import CFG
 
 
 if __name__ == '__main__':

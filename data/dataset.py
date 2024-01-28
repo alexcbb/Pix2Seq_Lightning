c4c9af50f1e2203001f8043ac81dc0a5f073b52d
alexcbb <alexchapin@hotmail.fr>
2024-01-28 17:41:57 +0100

    Fix issue transform bbox

--- data/dataset.py
+++ data/dataset.py
@@ -29,6 +29,13 @@ class YCBDataset(Dataset):
             gt_bbox = json.load(g)
             g.close()
             for scene in gt_pose.keys():
+                current_scene = {
+                        "bbox" : [], 
+                        "obj_class": [],
+                        "obj_trans": [],
+                        "obj_rot": [],
+                        "img_path": []
+                        }
                 for obj in range(len(gt_pose[scene])):
                     bbox = gt_bbox[scene][obj]['bbox_obj']
                     bbox = [bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3]] # xmin, ymin, xmax, ymax
@@ -36,35 +43,32 @@ class YCBDataset(Dataset):
                     obj_trans = gt_pose[scene][obj]['cam_t_m2c']
                     obj_rot = gt_pose[scene][obj]['cam_R_m2c']
                     img_path = scenes_train[i] + 'rgb/' + str(scene).zfill(6) + '.png'
-                    self.labels.append(
-                        {
-                        "bbox" : bbox, 
-                        "obj_class": obj_class,
-                        "obj_trans": obj_trans,
-                        "obj_rot": obj_rot,
-                        "img_path": img_path
-                        }
-                    )
+                    current_scene['bbox'].append(bbox)
+                    current_scene['obj_class'].append(obj_class)
+                    current_scene['obj_trans'].append(obj_trans)
+                    current_scene['obj_rot'].append(obj_rot)
+                    current_scene['img_path'].append(img_path)
+                self.labels.append(current_scene)
         self.transforms = transforms
         self.tokenizer = tokenizer
         print(f"Load dataset at path {path_to_dataset} with size: {len(self.labels)}")
 
     def __getitem__(self, idx):
         sample = self.labels[idx]
-        img_path = sample['img_path']
+        img_path = sample['img_path'][0]
 
         img = cv2.imread(img_path)[..., ::-1]
-        obj_class = sample['obj_class']
         bboxes = sample['bbox']
+        obj_class = sample['obj_class']
 
         print(f"Current sample {idx} with image path {img_path} and object class {obj_class} and bbox {bboxes}")
 
         if self.transforms is not None:
-            transformed = self.transforms(**{
-                'image': img,
-                'bboxes': [bboxes],
-                'obj_class': obj_class
-            })
+            transformed = self.transforms(
+                image=img,
+                bboxes=bboxes,
+                class_labels=obj_class
+            )
             img = transformed['image']
             bboxes = transformed['bboxes']
             obj_class = transformed['obj_class']

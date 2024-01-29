779fbb31867f79a6bd07e462b42b74b1ee4b925d
alexcbb <alexchapin@hotmail.fr>
2024-01-29 08:45:40 +0100

    Change clamp

--- data/dataset.py
+++ data/dataset.py
@@ -38,7 +38,7 @@ class YCBDataset(Dataset):
                         }
                 for obj in range(len(gt_pose[scene])):
                     bbox = gt_bbox[scene][obj]['bbox_obj']
-                    bbox = [max(0, bbox[0]), max(0, bbox[1]), min(bbox[0] + bbox[2], 640), min(bbox[1] + bbox[3], 480)] # xmin, ymin, xmax, ymax
+                    bbox = [max(1, bbox[0]), max(1, bbox[1]), min(bbox[0] + bbox[2], 639), min(bbox[1] + bbox[3], 479)] # xmin, ymin, xmax, ymax
                     obj_class= gt_pose[scene][obj]['obj_id']
                     obj_trans = gt_pose[scene][obj]['cam_t_m2c']
                     obj_rot = gt_pose[scene][obj]['cam_R_m2c']
